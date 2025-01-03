9be5dfbfc45fdf84f0cec576f53b9732e3ea8eda
Vadim Kim <vkim0203@gmail.com>
2025-01-03 15:14:24 +0900

    Timer Challenge components added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,18 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const newPlayerName = useRef();
+  const [playerName, setPlayerName] = useState("");
+
+  function handleClick() {
+    setPlayerName(newPlayerName.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName || "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={newPlayerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
